e7b42d3daf507feb722a2fe432527946dd2beb74
Riya Reddy <riyareddy331@gmail.com>
2025-06-30 19:25:06 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [food_list, setFoodList] = useState([]);
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-0xz4.onrender.com";
   const [token, setToken] = useState(localStorage.getItem("token") || "");
 
   // Fetch food list
