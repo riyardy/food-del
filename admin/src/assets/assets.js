e5c95aec0247135f49875701798612d56b2fc1ad
Riya Reddy <riyareddy331@gmail.com>
2025-06-30 19:27:22 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,9 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url="https://food-del-backend-0xz4.onrender.com"
+export const currency = '₹'
+
 export const assets ={
     logo,
     add_icon,
